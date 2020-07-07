ca12462a2a6b32b1dc4c6f53035400f6c7dedd46
Vince Fabro <vince.fabro@techdata.com>
2020-07-07 09:21:03 -0700

    Changed the shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,19 @@ do
    sleep $(echo "scale=4; $WAIT/1000" | bc)
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
+   MS=`date +%s%3N`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") M="$D ERROR An error is usually an exception that has been caught and not handled."
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") M="$D INFO This is less important than debug log and is often used to provide context in the current task."
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") M="$D WARN A warning that should be ignored is usually at this level and should be actionable."
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") M="$D DEBUG This is a debug log that shows a log that can be ignored."
       ;;
    esac
+
+   echo "{ \"_applicationId\":\"7280125\", \"_application\":\"Baggage Platform\", \"_filters\":[\"SRE\", \"SECURITY\", \"ANALYTICS\"], \"_timestamp\":\"$D\", \"_timeMillisInUTC\"=\"$MS\", \"_classification\":\"PII\", \"msg\":\"$M\" }"
+
    c=$(( c+1 ))
 done
